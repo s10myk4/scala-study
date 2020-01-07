9c18ca5c38d106aa39c5f9bcb2703cc3f7136ebd
Tomoyuki Shimomura <st4439@gmail.com>
2020-01-08 02:35:55 +0900

    done doobie exercise "connectiong to databse" section

---
+++ src/main/scala/doobie/ConnectingToDatabase.scala
@@ -0,0 +1,32 @@
+package doobie
+
+import cats.effect._
+import cats.implicits._
+import doobie.implicits._
+
+object ConnectingToDatabase extends MyIOApp {
+
+  def run(args: List[String]): IO[ExitCode] = {
+    val q1 = transactor.use(42.pure[ConnectionIO].transact[IO]).unsafeRunSync()
+
+    //run the query, interpret the resultset as a stream of Int values, and yield its one and only element.
+    val q2 = transactor.use(sql"select 42".query[Int].unique.transact[IO]).unsafeRunSync()
+
+    //compose two smaller programs
+    val program = for {
+      a <- sql"select 42".query[Int].unique
+      b <- sql"select power(5, 2)".query[Int].unique
+    } yield (a,b)
+    val q3 = transactor.use(program.transact[IO]).unsafeRunSync()
+
+    //Actually we need only an applicative functor in this case.
+    val oneProgram = sql"select 42".query[Int].unique
+    val anotherProgram = sql"select power(5, 2)".query[Int].unique
+    val q4 = transactor.use((oneProgram, anotherProgram).mapN(_ + _).transact[IO]).unsafeRunSync()
+
+    IO.pure(
+      asset(q1 == 42 && q2 == 42 && q3 == (42, 25) && q4 == 67)
+    )
+  }
+
+}
---
+++ src/main/scala/doobie/MyIOApp.scala
@@ -0,0 +1,26 @@
+package doobie
+
+import cats.effect.{Blocker, ExitCode, IO, IOApp, Resource}
+import doobie.hikari.HikariTransactor
+
+trait MyIOApp extends IOApp {
+
+  protected val transactor: Resource[IO, HikariTransactor[IO]] = for {
+    ce <- ExecutionContexts.fixedThreadPool[IO](32) //connect execution context
+    be <- Blocker[IO] //blocking execution context
+    xa <- HikariTransactor.newHikariTransactor[IO](
+      driverClassName = "org.h2.Driver",
+      url = "jdbc:h2:mem:test;DB_CLOSE_DELAY=-1",
+      user = "sa",
+      pass = "",
+      ce, // await connection here
+      be // execute JDBC operations here
+    )
+  } yield xa
+
+  protected def asset(f: => Boolean): ExitCode = {
+    if (f) ExitCode.Success
+    else ExitCode.Error
+  }
+
+}
